ca5b0cf557f686feff5bea69a59fd5362b3ae01a
suryass98 <suresh.ss98@gmail.com>
2020-09-25 01:05:50 +0200

    Creating project

---
+++ rule.engine/src/test/java/com/business/rule/engine/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.rule.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

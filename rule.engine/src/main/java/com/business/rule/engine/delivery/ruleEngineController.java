072b95118e720fc525b6b1de6e41851bce5336e2
suryass98 <suresh.ss98@gmail.com>
2020-09-25 02:16:42 +0200

    Creating tests for tdd approach

---
+++ rule.engine/src/main/java/com/business/rule/engine/delivery/ruleEngineController.java
@@ -0,0 +1,4 @@
+package com.business.rule.engine.delivery;
+
+public class ruleEngineController {
+}
---
+++ rule.engine/src/test/java/com/business/rule/engine/delivery/ruleEngineControllerTest.java
@@ -0,0 +1,23 @@
+package com.business.rule.engine.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(ruleEngineController.class)
+public class ruleEngineControllerTest {
+    @Test
+    public void testSuccessBOOK() throws Exception {
+
+    }
+    @Test
+    public void testSuccessPHYSICAL_PRODUCT() throws Exception {
+
+    }
+    @Test
+    public void testSuccessMEMBERSHIP_ACTIVATE() throws Exception {
+
+    }
+    @Test
+    public void testSuccessMEMBERSHIP_UPGRADE() throws Exception {
+
+    }
